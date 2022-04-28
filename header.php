9ed7fb52e89e5f1e83f4ba5cb69f66426bd5bea8
mu_tomoya <muton.tm.714@gmail.com>
2022-04-28 20:43:07 +0900

    favicon設定

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TOMOLOGO</title>
+    <link rel="shortcut icon" href="<?php bloginfo('template_url')?>/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/css/destyle.css" type="text/css">
     <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/style.css" type="text/css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
