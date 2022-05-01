5202ae69e29c9447fc1110c510202d3965aed9f2
mu_tomoya <muton.tm.714@gmail.com>
2022-05-01 13:06:07 +0900

    GA4設置

--- header.php
+++ header.php
@@ -1,3 +1,13 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-SKS4WK6G29"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-SKS4WK6G29');
+</script>
+
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
