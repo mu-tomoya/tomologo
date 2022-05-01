f68885baab1565e945f88d2e8835dbd7d1b900bb
mu_tomoya <muton.tm.714@gmail.com>
2022-05-01 14:36:16 +0900

    footerリンク修正　個別ページリンクレイアウト修正

--- include/footer.php
+++ include/footer.php
@@ -2,9 +2,9 @@
     <img class="logo" src="<?php echo esc_url(get_template_directory_uri()); ?>/images/logo-white.png" alt="logo">
     <nav>
         <ul class="footer-nav">
-            <li class="footer-nav-item"><a href="/">ホーム</a></li>
-            <li class="footer-nav-item"><a href="/profile">プロフィール</a></li>
-            <li class="footer-nav-item"><a href="contact">お問い合わせ</a></li>
+            <li class="footer-nav-item"><a href="<?php echo esc_url(home_url('/'))?>">ホーム</a></li>
+            <li class="footer-nav-item"><a href="/<?php echo esc_url(home_url('/profile'))?>">プロフィール</a></li>
+            <li class="footer-nav-item"><a href="<?php echo esc_url(home_url('/contact'))?>">お問い合わせ</a></li>
         </ul>
     </nav>
     <p>Copyright 2022 - TOMOLOGO All Rights Reserved.</p>
--- page-contact.php
+++ page-contact.php
@@ -9,10 +9,14 @@
 <body>
     <?php wp_head(); ?>
     <?php esc_html(get_template_part('include/header')) ?>
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <?php the_content(); ?>
-    <?php endwhile;
-    endif; ?>
+    <main class="main">
+        <div class="articles">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <?php the_content(); ?>
+            <?php endwhile;
+            endif; ?>
+        </div>
+    </main>
     <?php esc_html(get_template_part('include/footer')); ?>
     <?php wp_footer(); ?>
 </body>
